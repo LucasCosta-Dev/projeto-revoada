3cfce0d1925227e55bcd1fc2880f0f2433752de3
Lucas Costa <lucatcosta5@gmail.com>
2021-11-01 00:09:00 -0300

    cards

--- javascript/functions.js
+++ javascript/functions.js
@@ -3,25 +3,24 @@ var porcoes = document.querySelector(".porcoes");
 var sobremesas = document.querySelector(".sobremesas");
 
 function btnlanches() {
-  if (lanches.style.display === "none") {
-    lanches.style.display = "flex";
+  if (lanches.style.display = "none") {
+    lanches.style.display = "flex"; 
     porcoes.style.display = "none";
-    sobremesas.style.display = "none";
-  }else{
-    porcoes.style.display = "none"
+    sobremesas.style.display = "none";   
   }
 }
 function btnporcoes() {
-  if (porcoes.style.display === "none") {
-    lanches.style.display = "none";
+  if (porcoes.style.display = "none") {
     porcoes.style.display = "flex";
-    sobremesas.style.display = "none";
+    lanches.style.display = "none";
+    sobremesas.style.display = "none"
+
   }
 }
 function btnsobremesas() {
-  if (sobremesas.style.display === "none") {
+  if (sobremesas.style.display = "none") {
+    sobremesas.style.display = "flex";
     lanches.style.display = "none";
     porcoes.style.display = "none";
-    sobremesas.style.display = "flex";
   }
 }
