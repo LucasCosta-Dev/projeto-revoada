833e2cfeb3d6aea7f89e2a2740563a1e804178a0
Lucas Costa <lucatcosta5@gmail.com>
2021-10-31 21:47:09 -0300

    atualizacao

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,2 +1,27 @@
+var lanches = document.querySelector(".lanches");
+var porcoes = document.querySelector(".porcoes");
+var sobremesas = document.querySelector(".sobremesas");
 
-
+function btnlanches() {
+  if (lanches.style.display === "none") {
+    lanches.style.display = "flex";
+    porcoes.style.display = "none";
+    sobremesas.style.display = "none";
+  }else{
+    porcoes.style.display = "none"
+  }
+}
+function btnporcoes() {
+  if (porcoes.style.display === "none") {
+    lanches.style.display = "none";
+    porcoes.style.display = "flex";
+    sobremesas.style.display = "none";
+  }
+}
+function btnsobremesas() {
+  if (sobremesas.style.display === "none") {
+    lanches.style.display = "none";
+    porcoes.style.display = "none";
+    sobremesas.style.display = "flex";
+  }
+}
